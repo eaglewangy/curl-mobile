62528cfdb9d6444952121d867605e256af5a5d53
wyylling <wyylling@gmail.com>
2016-10-30 17:08:26 +0800

    init commit

---
+++ jni/Application.mk
@@ -0,0 +1,9 @@
+APP_OPTIM := release
+APP_PLATFORM := android-14
+APP_STL := gnustl_static
+APP_CPPFLAGS += -frtti
+APP_CPPFLAGS += -fexceptions
+APP_CPPFLAGS += -DANDROID
+APP_ABI := arm64-v8a
+APP_MODULES := curl
+NDK_TOOLCHAIN_VERSION := clang
